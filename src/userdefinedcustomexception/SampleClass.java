669a8e7deb63f51f526ea93ca7ba26458f801eaa
chandan-chintu <109058973+chandan-chintu@users.noreply.github.com>
2025-05-13 21:13:58 +0530

    Update SampleClass.java

--- src/userdefinedcustomexception/SampleClass.java
+++ src/userdefinedcustomexception/SampleClass.java
@@ -6,8 +6,11 @@ public class SampleClass {
             int a=100;
             if(a==100){
                 throw new UserNotFoundException("a is 100 and it is user defined custom exception");
+
+                // added new code
             }
         } catch (Exception e) {
+             // added new code
             System.out.println("Exception occurred : "+e.getMessage());
         }
     }
